0d8607d06cf6154dfb13cc1164948a824076f480
jjh <joojinhyun00@gmail.com>
2022-05-14 01:58:30 +0900

    메뉴 아이템 추가

--- app/src/main/java/blog/jinhyun/web/MainActivity.kt
+++ app/src/main/java/blog/jinhyun/web/MainActivity.kt
@@ -2,6 +2,8 @@ package blog.jinhyun.web
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.inputmethod.EditorInfo
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -30,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         // `Url`를 전달하여 페이지 로드
-        binding.webView.loadUrl("https://www.google.com/")
+        binding.webView.loadUrl("https://www.google.com")
 
         binding.editTextTextUri.setOnEditorActionListener { _, actionId, _ ->
             // 설정한 키보드 액션(imeOptions)이 작동했다면 입력된 `url`을 웹뷰에 로드
@@ -41,6 +43,32 @@ class MainActivity : AppCompatActivity() {
                 false
             }
         }
+    }
+
+    /**
+     * 선택 메뉴 생성
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // 메뉴 리소스 지정
+        menuInflater.inflate(R.menu.main, menu)
+        // `true`: 액티비티에게 메뉴가 있다고 인식
+        return true
+    }
+
+    /**
+     * 선택 메뉴의 아이템 선택시 호출되는 함수
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.itemHome, R.id.itemGoogle -> {
+                binding.webView.loadUrl("https://www.google.com")
+                return true
+            }
+            R.id.itemNaver -> {
+                binding.webView.loadUrl("https://www.naver.com")
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     /**
